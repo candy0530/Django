a8ff221b4ff1098f8c1dbbc583290e7c233255d9
Candy <candy@tangjiameideMacBook-Pro.local>
2015-06-05 11:39:11 +0800

    my djangogirls app

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from trips.views import hello_world, home, post_detail
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', 'trips.views.hello_world'),
+    url(r'^$', 'trips.views.home'),
+    url(r'^post/(?P<id>\d+)/$', post_detail, name='post_detail'),
+)
